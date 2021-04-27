3ba4064a0f3dfb87f5af90ef3d30d5c3ceb28e02
DESKTOP-BUMAKAD\82104 <thtto77@gmail.com>
2021-04-27 13:17:18 +0900

    application

---
+++ src/main/java/board/Board2OriApplication.java
@@ -0,0 +1,13 @@
+package board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board2OriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board2OriApplication.class, args);
+	}
+
+}
