445c95b991d1b233f864b28c140bc40c595890dc
DESKTOP-BUMAKAD\82104 <thtto77@gmail.com>
2021-04-27 13:10:37 +0900

    BoardDto

---
+++ src/main/java/board/dto/RestBoardDto.java
@@ -0,0 +1,16 @@
+package board.dto;
+
+import lombok.Data;
+
+@Data
+public class RestBoardDto {
+	private int boardIdx;
+	private String title;
+	private String contents;
+	private int hitCnt;
+	private String createdId;
+	private String createdDatetime;
+	private String updaterId;
+	private String updatedDatetime;
+	private String deletedYn;
+}
